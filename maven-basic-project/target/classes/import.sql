2da1dd48169c08975688befb6257aa6163cdf2a5
js834149 <js834149@gmail.com>
2026-03-12 11:44:43 +0100

    Add SQL import script for user data initialization

---
+++ maven-basic-project/target/classes/import.sql
@@ -0,0 +1,2 @@
+INSERT INTO users (username, email, password_hash, created_at) VALUES ('usuario1', 'user1@email.com', '1234', CURRENT_TIMESTAMP);
+INSERT INTO users (username, email, password_hash, created_at) VALUES ('usuario2', 'user2@email.com', '1234', CURRENT_TIMESTAMP);
