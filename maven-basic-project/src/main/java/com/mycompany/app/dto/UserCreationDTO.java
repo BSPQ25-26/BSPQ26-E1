4d1c96e65357d2e5238a70c04050555f1a363ab0
pablo10diez2 <pablo.diez@opendeusto.es>
2026-03-16 16:01:13 +0100

    Minor changes before merge

--- maven-basic-project/src/main/java/com/mycompany/app/dto/UserCreationDTO.java
+++ maven-basic-project/src/main/java/com/mycompany/app/dto/UserCreationDTO.java
@@ -4,11 +4,13 @@ public class UserCreationDTO {
     private String username;
     private String email;
     private String password;
+    private Double balance;
 
-    public UserCreationDTO(String username, String email, String password){
+    public UserCreationDTO(String username, String email, String password, Double balance){
         this.username = username;
         this.email = email;
         this.password = password;
+        this.balance = balance;
     }
 
     public String getUsername() {
@@ -33,5 +35,13 @@ public class UserCreationDTO {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public Double getBalance(){
+        return balance;
+    }
+
+    public void setBalance(Double balance){
+        this.balance = balance;
     }
 }
