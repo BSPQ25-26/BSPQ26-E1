2c7c2dd6cdc3437a6558b886edf6bf29ce6e4300
asieramigorena <asier.amigorena@gmail.com>
2026-03-24 17:38:32 +0100

    Adds transaction page

---
+++ maven-basic-project/src/main/java/com/mycompany/app/facade/TransactionViewController.java
@@ -0,0 +1,18 @@
+package com.mycompany.app.facade;
+
+import com.mycompany.app.service.AuthService;
+import com.mycompany.app.service.TransactionService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/web/transaction")
+public class TransactionViewController {
+
+    @GetMapping()
+    public String transactions(){
+        return "transactions";
+    }
+
+}
