08d36609f4224c920464e1ba7abe4882d9d6aba5
pablo10diez2 <pablo.diez@opendeusto.es>
2026-04-22 16:39:18 +0200

    Debt feature working

---
+++ maven-basic-project/src/main/java/com/mycompany/app/repository/DeudaRepository.java
@@ -0,0 +1,14 @@
+package com.mycompany.app.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.mycompany.app.model.Deuda;
+
+@Repository
+public interface DeudaRepository extends JpaRepository<Deuda, Integer> {
+    List<Deuda> findByDeudorId(Integer usuarioId);
+    List<Deuda> findByTransaccionOriginalId(Integer transaccionId);
+}
--- maven-basic-project/src/main/java/com/mycompany/app/repository/GroupRepository.java
+++ maven-basic-project/src/main/java/com/mycompany/app/repository/GroupRepository.java
@@ -1,17 +1,19 @@
 package com.mycompany.app.repository;
 
-import com.mycompany.app.model.Group;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.Optional;
+import com.mycompany.app.model.Group;
 
 @Repository
 public interface GroupRepository extends JpaRepository<Group, Integer> {
 
+    @Override
     Optional<Group> findById(Integer id);
 
     @Query("SELECT g FROM Group g JOIN g.miembros m WHERE m.email = :email")
